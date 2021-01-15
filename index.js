1a5e58f02228fa214c1f65f41be684d8944d48ef
Ahmad Hazman <ahmd.hazman@gmail.com>
2021-01-15 13:06:47 +0800

    latest changes

--- index.js
+++ index.js
@@ -12,13 +12,14 @@ app.listen(port);
 
 //  REQUEST BODY
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 //  DEFINE ROUTES
 app.get('/index', async (req, res) => {
 	res.send('Shotbook KBS APIs');
 });
 
-app.post('/getUser', async (req, res) => {
+app.post('/getUsers', async (req, res) => {
 	let response;
 	let rawString = '';
 
@@ -39,6 +40,11 @@ app.post('/getUser', async (req, res) => {
 				rawString += 'SELECT * ';
 				rawString += 'FROM kbs.users ';
 				rawString += 'WHERE venue_id = ' + req.body.venue_id;
+				break;
+			case 'shotbook_users':
+				rawString += 'SELECT * ';
+				rawString += 'FROM kbs.users ';
+				rawString += 'WHERE user_type_id = 3';
 				break;
 			default:
 				break;
@@ -104,6 +110,53 @@ app.post('/getFacilities', async (req, res) => {
 	}
 });
 
+app.post('/getAllFacilities', async (req, res) => {
+	let response;
+	let rawStringFacilities = '';
+	let rawStringVenues = '';
+	let result = [];
+
+	try {
+		rawStringVenues += 'SELECT id, name FROM kbs.venues ';
+		let venues = await dbconnection.query(rawStringVenues);
+
+		for (venue of venues.rows) {
+			rawStringFacilities = 'SELECT id, name FROM kbs.facilities ';
+			rawStringFacilities += 'WHERE venue_id=' + venue.id + ' ';
+			rawStringFacilities += `AND type='Subvenue' `;
+
+			let facilities = await dbconnection.query(rawStringFacilities);
+
+			let data = {
+				venue: {
+					venue_id: venue.id,
+					venue_name: venue.name,
+				},
+				facilities: facilities.rows,
+			};
+
+			result.push(data);
+		}
+
+		response = {
+			statusCode: 200,
+			message: 'Successful',
+			data: result,
+		};
+
+		return res.json(response);
+	} catch (err) {
+		response = {
+			statusCode: 500,
+			message: 'Error',
+			transaction: 'GET ALL FACILITIES',
+			data: req.body,
+		};
+		console.error(err.message);
+		return res.json(response);
+	}
+});
+
 app.post('/getVenues', async (req, res) => {
 	let response;
 	let rawString = '';
@@ -131,10 +184,6 @@ app.post('/getVenues', async (req, res) => {
 		};
 
 		return res.json(response);
-
-		// console.log('[Request Body] ', req.body);
-		// console.log('[Query] ', rawString);
-		// console.log('[Result] ', users.rows);
 	} catch (err) {
 		console.error(err.message);
 	}
@@ -142,6 +191,7 @@ app.post('/getVenues', async (req, res) => {
 
 app.post('/getEvents', async (req, res) => {
 	let response;
+	let result = [];
 	let rawString = '';
 
 	try {
@@ -152,23 +202,43 @@ app.post('/getEvents', async (req, res) => {
 		if (flag === 'single') {
 			rawString += 'SELECT * FROM ';
 			rawString += 'kbs.events ';
-			rawString += 'WHERE id = ' + venue_id;
-		} else if (flag === 'all') {
+			rawString += 'WHERE id = ' + event_id;
+		} else if (flag === 'byMonth') {
 			rawString += 'SELECT * ';
 			rawString += 'FROM kbs.events ';
 			rawString += 'WHERE EXTRACT(MONTH FROM start_date) = ' + month;
 		}
 
-		console.log(rawString);
-		// rawString += 'SELECT * FROM kbs.venues';
-
 		//	EXECUTE QUERIES ON DB
 		let events = await dbconnection.query(rawString);
+
+		if (events.rowCount > 0) {
+			for (event of events.rows) {
+				let rawStringFacility = 'SELECT * FROM ';
+				rawStringFacility += 'kbs.facilities ';
+				rawStringFacility += 'WHERE id = ' + event.facility_id;
+
+				let facility = await dbconnection.query(rawStringFacility);
+
+				let rawStringVenue = 'SELECT * FROM ';
+				rawStringVenue += 'kbs.venues ';
+				rawStringVenue += 'WHERE id = ' + facility.rows[0].venue_id;
+
+				let venue = await dbconnection.query(rawStringVenue);
+
+				let tempResult = {
+					event: event,
+					venue: venue.rows[0],
+					facility: facility.rows[0],
+				};
+				result.push(tempResult);
+			}
+		}
 
 		response = {
 			statusCode: 200,
 			message: 'Successful',
-			data: events.rows,
+			data: result,
 		};
 
 		return res.json(response);
@@ -203,8 +273,6 @@ app.post('/insertVenue', async (req, res) => {
 			'kbs.venues(name, employee_no, status, line_add_1, line_add_2, postal_code, city, state, tel_no, contact_person, contact_no, contact_email) ';
 		rawString += `VALUES('${name}', ${employee_no}, '${status}', '${line_add_1}', '${line_add_2}', ${postal_code}, '${city}', '${state}', ${tel_no}, '${contact_person}', ${contact_no}, '${contact_email}'`;
 		rawString += ')';
-
-		console.log('Raw String: ', rawString);
 
 		//	EXECUTE QUERIES ON DB
 		let venue = await dbconnection.query(rawString);
@@ -271,7 +339,6 @@ app.post('/insertfacility', async (req, res) => {
 			sundayCloseTime,
 		} = req.body;
 
-		console.log(req.body);
 		mondayStartTime === ''
 			? (mondayStartTime = null)
 			: (mondayStartTime = `'${mondayStartTime}'`);
@@ -314,7 +381,6 @@ app.post('/insertfacility', async (req, res) => {
 		sundayCloseTime === ''
 			? (sundayCloseTime = null)
 			: (sundayCloseTime = `'${sundayCloseTime}'`);
-		console.log({ facilityType });
 
 		if (facilityType === 'Subvenue') {
 			rawStringFacility = 'INSERT INTO ';
@@ -360,6 +426,55 @@ app.post('/insertfacility', async (req, res) => {
 			statusCode: 500,
 			message: 'Error',
 			transaction: 'INSERT VENUE',
+			data: req.body,
+		};
+
+		console.error(err.message);
+		return res.json(response);
+	}
+});
+
+app.post('/insertEvent', async (req, res) => {
+	let response;
+	let rawString = '';
+
+	try {
+		//	DESTRUCTURE REQUEST BODY
+		let {
+			name,
+			category,
+			description,
+			venue,
+			venue_id,
+			facility,
+			timestampStart,
+			timestampEnd,
+			type,
+		} = req.body;
+
+		rawString += 'INSERT INTO ';
+		rawString +=
+			'kbs.events(name, category, description, start_date, end_date, event_type, facility_id) ';
+		rawString += `VALUES('${name}', '${category}', '${description}', '${timestampStart}', '${timestampEnd}', '${type}', '${facility}')`;
+
+		console.log(rawString);
+		//	EXECUTE QUERIES ON DB
+		let event = await dbconnection.query(rawString);
+
+		response = {
+			statusCode: 200,
+			message: 'Successful',
+			transaction: event.command,
+			rows_inserted: event.rowCount,
+			data: event.rows,
+		};
+
+		return res.json(response);
+	} catch (err) {
+		response = {
+			statusCode: 500,
+			message: 'Error',
+			transaction: 'INSERT EVENT',
 			data: req.body,
 		};
 
